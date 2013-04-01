460a7f6bbffde440ae6e5c157ed9cf0fa331c021
Tod Cribb <tdcribb@gmail.com>
2013-04-01 09:19:42 -0400

    update to blog and reservation pages

--- wp-content/themes/jessicacribb/reservations.php
+++ wp-content/themes/jessicacribb/reservations.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<div id="form-cont">
-		<div class="form-title">Make a Reservation with Jessica</div>
+		<div class="form-title">Request a Reservation with Jessica</div>
 		<?php echo do_shortcode('[contact-form-7 id="74" title="Reservations Form"]'); ?>
 	</div>
 
