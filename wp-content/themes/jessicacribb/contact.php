f3a707ec3ffc7d0fc793719a461a1c4634906e1a
Tod Cribb <tdcribb@gmail.com>
2013-11-25 11:26:03 -0500

    udpate to addr and img

--- wp-content/themes/jessicacribb/contact.php
+++ wp-content/themes/jessicacribb/contact.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<div id="map_div"></div>
 		<script>
 			//GOOGLE MAPS//
-			var latlng = new google.maps.LatLng(34.172453,-79.816146);
+			var latlng = new google.maps.LatLng(33.873485,-79.755456);
 			var options = {
     			zoom: 15, // This number can be set to define the initial zoom level of the map
     			center: latlng,
@@ -30,7 +30,7 @@ get_header(); ?>
  			);
  			// Add Marker
  			var marker1 = new google.maps.Marker({
-    		 	position: new google.maps.LatLng(34.172453,-79.816146),
+    		 	position: new google.maps.LatLng(33.873485,-79.755456),
     		 	map: map,
     		 	icon: image // This path is the custom pin to be shown. Remove this line and the proceeding comma to use default pin
  			});
@@ -39,7 +39,7 @@ get_header(); ?>
     		});
 		
     		var infowindow1 = new google.maps.InfoWindow({
-    		    content:  createInfo('<div id="bublble-title">The Mane Attraction Salon</div>', '<div id="bubble-addr-1">1001 S Cashua Drive</div><div id="bubble-addr-2">Florence, SC 29501</div><div id="bubble-phone">(843) 555-5555</div>')
+    		    content:  createInfo('<div id="bublble-title">The Mane Attraction Salon</div>', '<div id="bubble-addr-1">127 West Main St</div><div id="bubble-addr-2">Lake City, SC 29560</div><div id="bubble-phone">(843) 374-4247</div>')
     		});
 			function createInfo(title, content) {
     		    return '<div class="infowindow"><strong>'+ title +'</strong>'+content+'</div>';
